d3ae227b3fcee3879352eafad0713495cd71b910
WiseyXD <aryan.s.nag@gmail.com>
2023-12-19 10:06:12 +0530

    MongoDB Successfully Connected

--- src/server.js
+++ src/server.js
@@ -5,7 +5,19 @@ const jwt = require("jsonwebtoken");
 const app = express();
 const PORT = 4000;
 
+mongoose.connect(
+    "mongodb+srv://WiseyXD:Qwerty88**@testcluster.hbkxnkx.mongodb.net/userapp"
+);
+
+
+
 app.use(express.json());
+
+app.post("/", (req, res) => {
+    res.status(201).json({
+        msg: "Created an User",
+    });
+});
 
 app.listen(PORT, () => {
     console.log("Server is Listening at " + PORT);
