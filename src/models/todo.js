2a028eb7d23650039107f3bc03b0b5482e0b8473
WiseyXD <aryan.s.nag@gmail.com>
2023-12-19 13:27:57 +0530

    CRUD Feature implemented

---
+++ src/models/todo.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const {Schema , model} = mongoose;
+const todoSchema = new Schema ({
+    title : {
+        type : String,
+        required : true,
+    },
+    completed : {
+        type : Boolean,
+        required : true,
+    },
+    description : {
+        type : String,
+        required : true,
+    },
+})
+const Todo = model('Todo',todoSchema);
+
+module.exports = Todo;
--- src/server.js
+++ src/server.js
@@ -5,19 +5,83 @@ const jwt = require("jsonwebtoken");
 const app = express();
 const PORT = 4000;
 
+const Todo = require("./models/todo");
+
 mongoose.connect(
     "mongodb+srv://WiseyXD:Qwerty88**@testcluster.hbkxnkx.mongodb.net/userapp"
 );
+
+async function readAllTodos()
+{
+    const data = await Todo.find({});
+    return data;
+}
+
+function createNewTodo(title , description , completed)
+{   
+    const newTodo = new Todo({
+        title,
+        description,
+        completed
+    });
+    newTodo.save();
+}
+
+async function searchById(title)
+{
+    const selectedTodo = await Todo.find({title : title});
+}
+
+async function deleteTodo(title)
+{
+    await Todo.deleteOne({title : title})
+}
+
+
+
 
 
 
 app.use(express.json());
 
+// read all todos
+app.get("/",async (req,res)=>{
+    const todos = await readAllTodos();
+    res.status(200).json({todos});
+})
+
+
+// Create new Todo
 app.post("/", (req, res) => {
+    const title = req.body.title;
+    const description = req.body.description;
+    const completed = req.body.completed;
+    createNewTodo(title,description,completed);
     res.status(201).json({
-        msg: "Created an User",
+        msg: "Created a Todo",
     });
 });
+
+// Update todo by title
+app.put("/",async (req,res)=>{
+    await Todo.updateOne({title : req.body.title},{
+        $set :{
+            completed : req.body.completed,
+        }
+    })
+    res.status(200).json({
+        msg : "UPDATE Done"
+    })
+})
+
+// Delete todo by title  
+app.delete("/",async(req,res)=>{
+    await Todo.deleteOne({title : req.body.title});
+    res.status(200).json({
+        msg : "DELETE Success"
+    })
+})
+
 
 app.listen(PORT, () => {
     console.log("Server is Listening at " + PORT);
