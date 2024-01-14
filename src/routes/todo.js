c4edcc4859cb11cb93de5aa4ec4a53dbfd8db0cd
WiseyXD <aryan.s.nag@gmail.com>
2024-01-14 12:42:23 +0530

    backend done

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authenticateToken = require("../middleware/user");
+const Todo = require("../models/todo");
 
 const {
     readAllTodos,
@@ -39,8 +40,35 @@ router.put("/completed/:id", (req, res) => {
 });
 
 // Update todo by id
-router.put("/update/:id", (req, res) => {});
+router.put("/update/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const existingTodo = await Todo.findById(id);
+        if (!existingTodo) {
+            return res.status(404).json({ error: "Todo not found" });
+        }
+        if (req.body.title) {
+            existingTodo.title = req.body.title;
+        }
+        if (req.body.completed !== undefined) {
+            existingTodo.completed = req.body.completed;
+        }
+        if (req.body.description) {
+            existingTodo.description = req.body.description;
+        }
+        if (req.body.resources) {
+            existingTodo.resources.push(...req.body.resources);
+        }
 
+        // Save the updated todo to the database
+        const updatedTodo = await existingTodo.save();
+
+        res.json(updatedTodo);
+    } catch (error) {
+        console.error("Error updating todo:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
 router.delete("/delete/:id", authenticateToken, async (req, res) => {
     deleteTodoById(req.params.id);
     res.status(200).json({
