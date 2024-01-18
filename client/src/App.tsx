25c9ff31be54746c1e1dbada72471b91b266bd59
WiseyXD <aryan.s.nag@gmail.com>
2024-01-18 19:30:04 +0530

    feat : Routing Done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { Button } from "./components/ui/button";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const isAuthorized = false;
 
     return (
         <>
-            Hello
-            <Button>Click Me</Button>
+            <Routes>
+                <Route path="/signup" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route
+                    path="/"
+                    element={
+                        isAuthorized ? <Home /> : <Navigate to={"/login"} />
+                    }
+                />
+            </Routes>
         </>
     );
 }
