51633cb12fc885d71eaecc11c0a60c6b872d31cc
WiseyXD <aryan.s.nag@gmail.com>
2024-01-18 20:35:45 +0530

    ui : Created Auth Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,22 +2,26 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
     const isAuthorized = false;
 
     return (
         <>
-            <Routes>
-                <Route path="/signup" element={<Register />} />
-                <Route path="/login" element={<Login />} />
-                <Route
-                    path="/"
-                    element={
-                        isAuthorized ? <Home /> : <Navigate to={"/login"} />
-                    }
-                />
-            </Routes>
+            <Navbar isAuthorized={isAuthorized} />
+            <div className="w-full max-w-[90%] mx-auto mt-4">
+                <Routes>
+                    <Route path="/signup" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route
+                        path="/"
+                        element={
+                            isAuthorized ? <Home /> : <Navigate to={"/login"} />
+                        }
+                    />
+                </Routes>
+            </div>
         </>
     );
 }
