f85447c35e2e441350eb84c4816fe24157413996
WiseyXD <aryan.s.nag@gmail.com>
2024-01-20 14:24:26 +0530

    ui : made necessary changes in ui on Todo Page

--- client/src/components/TodoCard.tsx
+++ client/src/components/TodoCard.tsx
@@ -20,13 +20,42 @@ import {
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover";
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+    SheetFooter,
+    SheetClose,
+    SheetOverlay,
+    SheetPortal,
+} from "@/components/ui/sheet";
+
 import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "./ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useToast } from "./ui/use-toast";
 import { TrashIcon, Pencil2Icon } from "@radix-ui/react-icons";
 
 export default function TodoCard() {
+    const { toast } = useToast();
+
+    function handleDelete() {
+        toast({
+            title: "Todo Deleted",
+        });
+    }
+
+    function handleUpdate() {
+        toast({
+            title: "Todo Updated",
+            description: "Update all the changes",
+        });
+    }
+
     return (
         <Card className="hover:bg-gray-300 transition duration-100 ease-in-out ">
             <CardHeader className="flex flex-row justify-between">
@@ -99,9 +128,62 @@ export default function TodoCard() {
                     </DialogContent>
                 </Dialog>
             </CardContent>
-            <CardFooter className="flex justify-end">
-                <TrashIcon width="30" height="20" />
-                <Pencil2Icon width="22" height="18" />
+            <CardFooter className="flex justify-end gap-3">
+                {/* <Button>
+                    <Pencil2Icon width="30" height="20" />
+                </Button> */}
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button>
+                            {" "}
+                            <Pencil2Icon width="30" height="20" />
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent>
+                        <SheetHeader>
+                            <SheetTitle>Edit Todo</SheetTitle>
+                            <SheetDescription>
+                                Make changes to your Todo here. Click save when
+                                you're done.
+                            </SheetDescription>
+                        </SheetHeader>
+                        <div className="grid gap-4 py-4">
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label htmlFor="title" className="text-right">
+                                    Title
+                                </Label>
+                                <Input
+                                    id="title"
+                                    value="TodoTitle"
+                                    className="col-span-3"
+                                />
+                            </div>
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label
+                                    htmlFor="description"
+                                    className="text-right"
+                                >
+                                    Description
+                                </Label>
+                                <Input
+                                    id="description"
+                                    value="tododeascription"
+                                    className="col-span-3"
+                                />
+                            </div>
+                        </div>
+                        <SheetFooter>
+                            <SheetClose asChild>
+                                <Button type="submit" onClick={handleUpdate}>
+                                    Save changes
+                                </Button>
+                            </SheetClose>
+                        </SheetFooter>
+                    </SheetContent>
+                </Sheet>
+                <Button onClick={handleDelete}>
+                    <TrashIcon width="30" height="20" />
+                </Button>
             </CardFooter>
         </Card>
     );
