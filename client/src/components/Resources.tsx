b1c2a245b502b6a52a2255989b372426c963952c
WiseyXD <aryan.s.nag@gmail.com>
2024-01-21 22:31:23 +0530

    build : add new resources

---
+++ client/src/components/Resources.tsx
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "./ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { toast, useToast } from "./ui/use-toast";
+import ListItem from "./ListItem";
+import { useUpdateResourceTodoMutation } from "@/app/api/todoApi";
+
+type TResourceProps = {
+    title: string;
+    _id: string;
+    resources: [
+        {
+            name: string;
+            link: string;
+        }
+    ];
+};
+
+export default function Resources({ title, _id, resources }: TResourceProps) {
+    const [resource, setResource] = useState({
+        name: "",
+        link: "",
+    });
+
+    const [resourceMutation] = useUpdateResourceTodoMutation();
+    async function handleSubmit() {
+        if (resource.name === "" || resource.link === "") {
+            toast({
+                title: "Please fill in all the fields",
+                variant: "destructive",
+            });
+            return;
+        }
+        const { data } = await resourceMutation({ _id, resource });
+        console.log(data);
+        toast({
+            title: resource.name + "resource Added Successfully",
+        });
+    }
+
+    return (
+        <Dialog>
+            <DialogTrigger>
+                <Button>Check Resources</Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>{title}</DialogTitle>
+                    <DialogDescription>
+                        {resources && (
+                            <ul className="flex flex-col">
+                                {resources.map(({ name, link }, i) => {
+                                    return (
+                                        <ListItem
+                                            key={i}
+                                            name={name}
+                                            link={link}
+                                        />
+                                    );
+                                })}
+                            </ul>
+                        )}
+                        <Popover>
+                            <PopoverTrigger asChild>
+                                <Button variant="outline">Add Resource</Button>
+                            </PopoverTrigger>
+                            <PopoverContent className="w-80">
+                                <div className="grid gap-4">
+                                    <div className="space-y-2">
+                                        <h4 className="font-medium leading-none">
+                                            New Resource
+                                        </h4>
+                                        <p className="text-sm text-muted-foreground">
+                                            Add a new resource link
+                                        </p>
+                                    </div>
+                                    <div className="grid gap-2">
+                                        <div className="grid grid-cols-3 items-center gap-4">
+                                            <Label htmlFor="name">Name</Label>
+                                            <Input
+                                                id="name"
+                                                placeholder="Resource Name"
+                                                className="col-span-2 h-8"
+                                                defaultValue={resource.name}
+                                                onChange={(e) => {
+                                                    setResource((prevData) => ({
+                                                        ...prevData,
+                                                        name: e.target.value,
+                                                    }));
+                                                }}
+                                            />
+                                        </div>
+                                        <div className="grid grid-cols-3 items-center gap-4">
+                                            <Label htmlFor="link">Link</Label>
+                                            <Input
+                                                id="link"
+                                                placeholder="Resource Link"
+                                                className="col-span-2 h-8"
+                                                defaultValue={resource.link}
+                                                onChange={(e) => {
+                                                    setResource((prevData) => ({
+                                                        ...prevData,
+                                                        link: e.target.value,
+                                                    }));
+                                                }}
+                                            />
+                                        </div>
+                                        <div className="grid grid-cols-1 items-center">
+                                            <Button onClick={handleSubmit}>
+                                                Add
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </PopoverContent>
+                        </Popover>
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    );
+}
--- client/src/components/TodoCard.tsx
+++ client/src/components/TodoCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import {
     Card,
     CardContent,
@@ -7,29 +7,14 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog";
-import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@/components/ui/popover";
 
 import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "./ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { useToast } from "./ui/use-toast";
-import { TrashIcon, Pencil2Icon } from "@radix-ui/react-icons";
-import ListItem from "./ListItem";
+import { TrashIcon } from "@radix-ui/react-icons";
 import { useCompletedTodoMutation } from "@/app/api/todoApi";
 import UpdateTodoSheet from "./UpdateTodoSheet";
+import Resources from "./Resources";
 
 export type TTodoCardProps = {
     description: string;
@@ -85,72 +70,7 @@ export default function TodoCard({
                 />
             </CardHeader>
             <CardContent>
-                <Dialog>
-                    <DialogTrigger>
-                        <Button>Check Resources</Button>
-                    </DialogTrigger>
-                    <DialogContent>
-                        <DialogHeader>
-                            <DialogTitle>{title}</DialogTitle>
-                            <DialogDescription>
-                                {resources && (
-                                    <ul className="flex flex-col">
-                                        {resources.map(({ name, link }, i) => {
-                                            return (
-                                                <ListItem
-                                                    key={i}
-                                                    name={name}
-                                                    link={link}
-                                                />
-                                            );
-                                        })}
-                                    </ul>
-                                )}
-                                <Popover>
-                                    <PopoverTrigger asChild>
-                                        <Button variant="outline">
-                                            Add Resource
-                                        </Button>
-                                    </PopoverTrigger>
-                                    <PopoverContent className="w-80">
-                                        <div className="grid gap-4">
-                                            <div className="space-y-2">
-                                                <h4 className="font-medium leading-none">
-                                                    New Resource
-                                                </h4>
-                                                <p className="text-sm text-muted-foreground">
-                                                    Add a new resource link
-                                                </p>
-                                            </div>
-                                            <div className="grid gap-2">
-                                                <div className="grid grid-cols-3 items-center gap-4">
-                                                    <Label htmlFor="name">
-                                                        Name
-                                                    </Label>
-                                                    <Input
-                                                        id="name"
-                                                        placeholder="Resource Name"
-                                                        className="col-span-2 h-8"
-                                                    />
-                                                </div>
-                                                <div className="grid grid-cols-3 items-center gap-4">
-                                                    <Label htmlFor="link">
-                                                        Link
-                                                    </Label>
-                                                    <Input
-                                                        id="link"
-                                                        placeholder="Resource Link"
-                                                        className="col-span-2 h-8"
-                                                    />
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </PopoverContent>
-                                </Popover>
-                            </DialogDescription>
-                        </DialogHeader>
-                    </DialogContent>
-                </Dialog>
+                <Resources title={title} _id={_id} resources={resources} />
             </CardContent>
             <CardFooter className="flex justify-end gap-3">
                 <UpdateTodoSheet
