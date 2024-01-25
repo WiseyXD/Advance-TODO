d0b01d5d14ca6c6ef8b48401262ca2595e04c370
WiseyXD <aryan.s.nag@gmail.com>
2024-01-25 21:26:44 +0530

    build : admin can delete todo

--- backend/src/routes/admin/adminActions.js
+++ backend/src/routes/admin/adminActions.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../../models/user");
 const Todo = require("../../models/todo");
 const authenticateToken = require("../../middleware/user");
+const { createNewTodo, deleteTodoById } = require("../../utils/CRUDhelper");
 
 // router.use(authenticateToken);
 
@@ -28,6 +29,53 @@ router.get("/todos/:userId", async (req, res) => {
         const todos = await Todo.find({ email: userEmail });
         res.status(200).json({
             todos,
+        });
+    } catch (error) {
+        const msg = error.message;
+        res.status(500).json({
+            msg,
+        });
+    }
+});
+
+router.post("/todos/create/:userId", async (req, res) => {
+    const { userId } = req.params;
+    const title = req.body.title;
+    const description = req.body.description;
+    const completed = false;
+    const resource = req.body.resource;
+    try {
+        const user = await User.findOne({ _id: userId });
+        const email = user.email;
+        const response = await createNewTodo(
+            email,
+            title,
+            description,
+            completed,
+            resource
+        );
+        if (typeof response === "string") {
+            return res.status(403).json({
+                response,
+            });
+        }
+        res.status(201).json({
+            response,
+        });
+    } catch (error) {
+        const msg = error.message;
+        res.status(500).json({
+            msg,
+        });
+    }
+});
+
+router.delete("/todos/delete/:todoId", async (req, res) => {
+    const { todoId } = req.params;
+    try {
+        deleteTodoById(todoId);
+        res.status(200).json({
+            msg: "DELETE Success",
         });
     } catch (error) {
         const msg = error.message;
